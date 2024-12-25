c3237da6d2d843c228b9e81cdf9124c5ee7b3987
DevKevinA <125840079+DevKevinA@users.noreply.github.com>
2024-12-25 01:47:39 -0300

    Update config.php

--- painel/Connections/config.php
+++ painel/Connections/config.php
@@ -5,6 +5,6 @@
 $hostname_config = "localhost";
 $database_config = "l2jdb";
 $username_config = "root";
-$password_config = "";
+$password_config = "root";
 $config = mysql_pconnect($hostname_config, $username_config, $password_config) or trigger_error(mysql_error(),E_USER_ERROR); 
 ?>
